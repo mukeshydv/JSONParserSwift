002934530da5f037547b78d19f464fdeb9c43a1d
Mukesh Yadav <mails4ymukesh@gmail.com>
2017-02-08 15:03:54 +0530

    Added parsing with Data

--- JSONParserSwift/Classes/JSONParserSwift.swift
+++ JSONParserSwift/Classes/JSONParserSwift.swift
@@ -13,17 +13,25 @@ class JSONParserSwift {
 	static func parse<Type: JSONParsable>(string: String) throws -> Type {
 		if let data = string.data(using: .utf8) {
 			do {
-				let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
-				if let dictionary = dictionary as? [String: Any] {
-					return Type(dictionary: dictionary)
-				} else {
-					throw JSONParserSwiftError.cannotParseJsonString
-				}
+				return try parse(data: data)
 			} catch {
 				throw error
 			}
 		} else {
 			throw JSONParserSwiftError.cannotParseJsonString
+		}
+	}
+	
+	static func parse<Type: JSONParsable>(data: Data) throws -> Type {
+		do {
+			let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+			if let dictionary = dictionary as? [String: Any] {
+				return Type(dictionary: dictionary)
+			} else {
+				throw JSONParserSwiftError.cannotParseJsonString
+			}
+		} catch {
+			throw error
 		}
 	}
 	
