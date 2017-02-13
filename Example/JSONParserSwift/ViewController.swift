eab578a94e11939eca03ead5a1f2655a57ee7d1c
Mukesh Yadav <mails4ymukesh@gmail.com>
2017-02-13 15:36:54 +0530

    Array at root node support added

--- Example/JSONParserSwift/ViewController.swift
+++ Example/JSONParserSwift/ViewController.swift
@@ -11,9 +11,35 @@ import JSONParserSwift
 
 class ViewController: UIViewController {
 
+	let jsonString = "[" +
+		"{\"test\": \"Test Data 1\"}," +
+		"{\"test\": \"Test Data 2\"}," +
+		"{\"test\": \"Test Data 3\"}," +
+		"{\"test\": \"Test Data 4\"}" +
+	"]"
+	
+	let arrayOfNumbers = "[101, 102, 103, 104]"
+	
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+		do {
+			let array: Array<TestModel> = try JSONParserSwift.parse(string: jsonString)
+			print(array)
+			
+			if let data = jsonString.data(using: .utf8) {
+				let parsedArray: Array<TestModel> = try JSONParserSwift.parse(data: data)
+				print(parsedArray)
+			}
+			
+			if let data = arrayOfNumbers.data(using: .utf8) {
+				let parsedNumbers = try JSONParserSwift.parse(data: data)
+				print(parsedNumbers)
+			}
+			
+		} catch {
+			print(error)
+		}
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +47,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
+
+class TestModel: ParsableModel {
+	var test: String?
 }
 
--- JSONParserSwift/Classes/JSONParserSwift.swift
+++ JSONParserSwift/Classes/JSONParserSwift.swift
@@ -19,6 +19,7 @@ public class JSONParserSwift {
 	///		print(parseError)
 	///	}
 	///```
+	///
 	/// - Parameter string: string representation of JSON.
 	/// - Returns: Returns the parsed object. The type of object must be given explicitly at calling time of the method.
 	/// - Throws: Throws an error if given string is not a valid JSON.
@@ -26,6 +27,55 @@ public class JSONParserSwift {
 		if let data = string.data(using: .utf8) {
 			do {
 				return try parse(data: data)
+			} catch {
+				throw error
+			}
+		} else {
+			throw JSONParserSwiftError.cannotParseJsonString
+		}
+	}
+	
+	/// Use this method to parse the string format of JSON into array of Numbers or Strings.
+	///```
+	///	do {
+	///		let responseArray = try JSONParserSwift.parse(string: jsonArrayString)
+	///	} catch let parseError {
+	///		print(parseError)
+	///	}
+	///```
+	///
+	/// - Parameter string: string representation of JSON.
+	/// - Returns: Returns the parsed array.
+	/// - Throws: Throws an error if given string is not a valid JSON.
+	public static func parse(string: String) throws -> Array<Any> {
+		if let data = string.data(using: .utf8) {
+			do {
+				return try parse(data: data)
+			} catch {
+				throw error
+			}
+		} else {
+			throw JSONParserSwiftError.cannotParseJsonString
+		}
+	}
+	
+	/// Use this method to parse the string format of JSON into array of Parsable Model.
+	///```
+	///	do {
+	///		let responseArray: Array<YourModel> = try JSONParserSwift.parse(string: jsonArrayString)
+	///	} catch let parseError {
+	///		print(parseError)
+	///	}
+	///```
+	///
+	/// - Parameter string: string representation of JSON.
+	/// - Returns: Returns the parsed array.
+	/// - Throws: Throws an error if given string is not a valid JSON.
+	public static func parse<Type: ParsableModel>(string: String) throws -> Array<Type> {
+		if let data = string.data(using: .utf8) {
+			do {
+				let result: Array<Type> = try parse(data: data)
+				return result
 			} catch {
 				throw error
 			}
@@ -42,6 +92,7 @@ public class JSONParserSwift {
 	///		print(parseError)
 	///	}
 	///```
+	///
 	/// - Parameter data: data represenatation of the JSON.
 	/// - Returns: Returns the parsed object. The type of object must be given explicitly at calling time of the method.
 	/// - Throws: Throws an error if given data is not a valid JSON.
@@ -58,6 +109,57 @@ public class JSONParserSwift {
 		}
 	}
 	
+	/// Use this method to parse the `Data` representation of the JSON into array of Numbers or Strings.
+	///```
+	///	do {
+	///		let responseArray: Array<Any> = try JSONParserSwift.parse(data: jsonArrayData)
+	///	} catch let parseError {
+	///		print(parseError)
+	///	}
+	///```
+	///
+	/// - Parameter data: data represenatation of the JSON.
+	/// - Returns: Returns the parsed object.
+	/// - Throws: Throws an error if given data is not a valid JSON.
+	public static func parse(data: Data) throws -> Array<Any> {
+		do {
+			let array = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+			if let array = array as? [Any] {
+				return try parse(array: array)
+			} else {
+				throw JSONParserSwiftError.cannotParseJsonString
+			}
+		} catch {
+			throw error
+		}
+	}
+	
+	/// Use this method to parse the `Data` representation of the JSON into Parsable Model array.
+	///```
+	///	do {
+	///		let responseArray: Array<YourModel> = try JSONParserSwift.parse(data: jsonArrayData)
+	///	} catch let parseError {
+	///		print(parseError)
+	///	}
+	///```
+	///
+	/// - Parameter data: data represenatation of the JSON.
+	/// - Returns: Returns the parsed object.
+	/// - Throws: Throws an error if given data is not a valid JSON.
+	public static func parse<Type: ParsableModel>(data: Data) throws -> Array<Type> {
+		do {
+			let array = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+			if let array = array as? [[String: Any]] {
+				let result: Array<Type> = try parse(array: array)
+				return result
+			} else {
+				throw JSONParserSwiftError.cannotParseJsonString
+			}
+		} catch {
+			throw error
+		}
+	}
+	
 	/// Use this method to parse the `Dictionary` into Objects.
 	///
 	/// - Parameter dictionary: dictionary represenatation of the JSON.
@@ -66,8 +168,42 @@ public class JSONParserSwift {
 		return Type(dictionary: dictionary)
 	}
 	
+	/// Use this method to parse the `Array` into array of Numbers or Strings.
+	///
+	/// - Parameter array: array represenatation of the JSON.
+	/// - Returns: Returns the parsed object array.
+	/// - Throws: Throws an error if given array is not of Numbers or Strings.
+	public static func parse(array: [Any]) throws -> Array<Any> {
+		var resultArray = [Any]()
+		if (array.count > 0) {
+			if let firstElement = array.first as? [String: Any] {
+				throw JSONParserSwiftError.invalidJsonArrayType("Unexpected dictionary type: " + firstElement.description)
+			} else {
+				for element in array {
+					resultArray.append(element)
+				}
+			}
+		}
+		return resultArray
+	}
+	
+	/// Use this method to parse the `Array` into array of Parsable Models.
+	///
+	/// - Parameter array: array represenatation of the JSON.
+	/// - Returns: Returns the parsed object array.
+	public static func parse<Type: ParsableModel>(array: [[String: Any]]) -> Array<Type> {
+		var resultArray = [Type]()
+		if (array.count > 0) {
+			for element in array {
+				let parsedObject: Type = JSONParserSwift.parse(dictionary: element)
+				resultArray.append(parsedObject)
+			}
+		}
+		return resultArray
+	}
 }
 
 enum JSONParserSwiftError: Error {
 	case cannotParseJsonString
+	case invalidJsonArrayType(String)
 }
